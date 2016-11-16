6a3db9ed988185071e77ef24c96360c45888045c
Gregory Getchell <ggetchell@gmail.com>
2016-11-16 06:32:20 -0500

    First Commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+A program also created as a project for the freecodecamp.com Front-End-Developer course. Using Codepen.io and written in HTML CSS and Javascript, a simple Wikipedia viewer, nothing fancy it allows you to type in a keyword and will link it to the internet search. It can be  found at <a href="http://www.ggetchell.com/zipline-build-a-wikipedia-viewer/index.html">Wikipedia Viewer</a>
+
+ 
---
+++ js/index.js
@@ -0,0 +1,21 @@
+$('#search-form').submit(function(e){
+	e.preventDefault();
+});
+
+function search(){
+$("#display").empty();
+
+	sq = $('#srch').val();
+
+	$.getJSON("https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch="+sq+"&srnamespace=0&srwhat=text&titles=Main%20Page&callback=?", function(d) {
+
+		$.each(d.query.search, function(i, item){
+			
+var wiki = '<div class="entry">'+
+	'<div class="title">'+item.title+'</div>'+
+	'<div class="snippet">'+item.snippet+'</div></div>';
+			$("#display").append(wiki);
+		});
+	});
+$('#srch').val('');
+}
